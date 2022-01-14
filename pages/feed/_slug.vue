26c5e785b0b35ecff909436f9559c9ca066b922f
Isaac Chabon <ichabon@me.com>
2022-01-14 10:55:14 -0500

    rename blog to feed

---
+++ pages/feed/_slug.vue
@@ -0,0 +1,14 @@
+<template>
+  <article>
+    <nuxt-content :document="post" />
+  </article>
+</template>
+
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const post = await $content('feed', params.slug).fetch()
+      return { post }
+    }
+  }
+</script>
---
+++ pages/feed/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <NuxtLink
+      v-for="post in posts"
+      :key="post.slug"
+      :to="post.path"
+      v-html="post.title"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const posts = await $content('feed').fetch()
+    return {
+      posts
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
